ae637afe845326d6498ebc2df538997d32aa632d
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2022-02-21 23:34:57 -0500

    format ShoppingList

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./styles.css";
 import Header from "./components/Header";
-
+import ShoppingList from "./components/ShoppingList";
 import AddForm from "./components/AddForm";
+import { useState } from "react";
 
 export default function App() {
+  const [shoppingList, setShoppingList] = useState([
+    { name: "bread", quantity: 5, price: 1.99 }
+  ]);
   return (
     <div className="App">
       <Header></Header>
       <div>
         <AddForm></AddForm>
       </div>
-      <div></div>
+      <div>
+        <ShoppingList shoppingList={shoppingList}></ShoppingList>
+      </div>
     </div>
   );
 }
--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -6,43 +6,53 @@ export default function ShoppingList(props) {
     <div className="bg-light m-3 border border-light rounded">
       <div className="row m-2 mb-3 text-center">
         <span className="col-3 align-middle">Product name</span>
-        <span className="col-3 align-middle">Price</span>
         <span className="col-3 align-middle">Quantity</span>
+        <span className="col-4 align-middle"></span>
+        <span className="col-2 text-end">Price</span>
       </div>
 
-      {shoppingList.length === 0 && <div>Shopping list is empty.</div>}
+      {shoppingList.length === 0 && (
+        <div className="mb-2">Shopping list is empty.</div>
+      )}
       {shoppingList.map((product) => (
         <div key={product.id} className="row m-2">
           <span className="col-3 align-middle">{product.name}</span>
-          <span className="col-3 align-middle">{product.price}</span>
-          <span className="col-3 align-middle">{product.quantity}</span>
-          <span className="col-2 align-middle">
-            <button
-              type="button"
-              onClick={() => onIncrement(product)}
-              className="btn btn-primary font-weight-bold btn-sm me-1"
-            >
-              {" "}
-              +{" "}
-            </button>
-            <button
-              type="button"
-              onClick={() => onDecrement(product)}
-              className="btn btn-outline-primary font-weight-bold btn-sm align-middle"
-            >
-              {" "}
-              -{" "}
-            </button>
-          </span>
+          <div className="col-3 align-middle">
+            <span className="col-1 align-middle">
+              <button
+                type="button"
+                onClick={() => onDecrement(product)}
+                className="btn btn-outline-primary font-weight-bold btn-sm"
+              >
+                {" "}
+                -{" "}
+              </button>
+            </span>
+            <span className="col-1 text-center ms-3 me-3">
+              {product.quantity}
+            </span>
+            <span className="col-1 align-middle">
+              <button
+                type="button"
+                onClick={() => onIncrement(product)}
+                className="btn btn-primary font-weight-bold btn-sm"
+              >
+                {" "}
+                +{" "}
+              </button>
+            </span>
+          </div>
           <span className="col-1 align-middle">
             <button
               type="button"
               onClick={() => removeProduct(product)}
-              className="btn btn-warning"
+              className="btn btn-warning btn-sm"
             >
               Remove
             </button>
           </span>
+          <span className="col-3 align-middle"></span>
+          <span className="col-2 text-end">{product.price.toFixed(2)}</span>
         </div>
       ))}
     </div>
