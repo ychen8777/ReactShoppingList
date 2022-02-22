a86e12b8b1969b2d289a372218a3c671777265fd
ychen8777 <60375005+ychen8777@users.noreply.github.com>
2022-02-21 20:23:09 -0500

    update Product to functional component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,47 +1,13 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Product extends Component {
-  state = {
-    name: this.props.name,
-    price: this.props.price,
-    quantity: this.props.quantity
-  };
+export default function Product(props) {
+  const { product } = props;
 
-  handleIncrease = () => {
-    this.setState({ quantity: this.state.quantity + 1 });
-  };
-
-  handleDecrease = () => {
-    let newQuantity = this.state.quantity > 0 ? this.state.quantity - 1 : 0;
-    this.setState({ quantity: newQuantity });
-  };
-
-  render() {
-    console.log("props", this.props);
-    return (
-      <div className="row m-2">
-        <span className="col-3 align-middle">{this.state.name}</span>
-        <span className="col-3 align-middle">{this.state.price}</span>
-        <span className="col-3 align-middle">{this.state.quantity}</span>
-        <span className="col-2 align-middle">
-          <button
-            onClick={this.handleIncrease}
-            className="btn btn-primary font-weight-bold btn-sm me-1"
-          >
-            {" "}
-            +{" "}
-          </button>
-          <button
-            onClick={this.handleDecrease}
-            className="btn btn-outline-primary font-weight-bold btn-sm align-middle"
-          >
-            {" "}
-            -{" "}
-          </button>
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="row m-2">
+      <span className="col-3 align-middle">{product.name}</span>
+      <span className="col-3 align-middle">{product.price}</span>
+      <span className="col-3 align-middle">{product.quantity}</span>
+    </div>
+  );
 }
-
-export default Product;
